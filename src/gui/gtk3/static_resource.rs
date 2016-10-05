7899227a0df87a9c6a9c7464854f02a5beece07e
Stefan Müller <s.mueller@it.kls-glt.de>
2016-10-05 14:01:16 +0200

    Erste Version mit dem gresource File
    Dokumentation muss noch gemacht werden.
    s Bitte geben Sie eine Commit-Beschreibung für Ihre Änderungen ein. Zeilen,

---
+++ src/gui/gtk3/static_resource.rs
@@ -0,0 +1,12 @@
+extern crate gio;
+extern crate glib;
+extern crate libc;
+
+use std;
+
+pub fn init() {
+    let resource_data: &[u8] = &include_bytes!("../../resources/resources.gresource")[..];
+    let bytes = glib::Bytes::from(&resource_data);
+    let resource = gio::Resource::new_from_data(&bytes).unwrap();
+    gio::resources_register(&resource);
+}
