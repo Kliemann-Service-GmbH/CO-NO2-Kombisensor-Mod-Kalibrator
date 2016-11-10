5685781e1755e7ab324d89f956686008f52b4d6b
Stefan Müller <s.mueller@it.kls-glt.de>
2016-11-10 18:40:59 +0100

    Refactoring Kalibrations View, unfertig.

--- src/gui/gtk3/view_calibrator.rs
+++ src/gui/gtk3/view_calibrator.rs
@@ -6,6 +6,40 @@ use gtk;
 use gtk::prelude::*;
 
 
+fn fill_widgets(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>, sensor_num: usize) {
+    let button_messpunkt_nullgas: gtk::Button = builder.get_object("button_messpunkt_nullgas").unwrap();
+    let mut sensor_type: &str = "";
+    let label_sensor_type: gtk::Label = builder.get_object("label_sensor_type").unwrap();
+    if sensor_num == 0 {
+        label_sensor_type.set_text("Nemoto™ EC NAP-550 - NO2");
+        sensor_type = "NO2";
+    } else {
+        label_sensor_type.set_text("Nemoto™ EC NAP-505 - CO");
+        sensor_type = "CO";
+    }
+
+    {
+        let kombisensor = kombisensor.lock().unwrap();
+        let ref sensor = kombisensor.sensors[sensor_num];
+        let adjustment_sensor_minimal: gtk::Adjustment = builder.get_object("adjustment_sensor_minimal").unwrap();
+        let adjustment_sensor_maximal: gtk::Adjustment = builder.get_object("adjustment_sensor_maximal").unwrap();
+        let adjustment_sensor_concentration_at_nullgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_nullgas").unwrap();
+        let adjustment_sensor_concentration_at_messgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_messgas").unwrap();
+        let min_value = sensor.get_min_value() as f64;
+        let max_value = sensor.get_max_value() as f64 + 1.0;
+
+        adjustment_sensor_minimal.configure(min_value, min_value, max_value, 1.0, 1.0, 1.0);
+        adjustment_sensor_maximal.configure(max_value, min_value, max_value, 1.0, 1.0, 1.0);
+        adjustment_sensor_concentration_at_nullgas.configure(0.0, min_value, max_value, 1.0, 1.0, 1.0);
+        adjustment_sensor_concentration_at_messgas.configure(sensor.get_concentration_at_messgas() as f64,
+            min_value, max_value, 1.0, 1.0, 1.0);
+    }
+
+    button_messpunkt_nullgas.connect_clicked(clone!(builder, kombisensor => move |_| {
+        callback_button_messpunkt_nullgas(&sensor_type, &builder, &kombisensor);
+    }));
+}
+
 fn callback_button_messpunkt_nullgas<T: AsRef<str>>(sensor_type: T, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
     ::gui::gtk3::view_messpunkt::launch(sensor_type, &builder, &kombisensor);
 }
@@ -16,57 +50,21 @@ pub fn launch<T: AsRef<str>>(sensor_type: T, builder: &gtk::Builder, kombisensor
     let box_calibrator_view: gtk::Box = builder.get_object("box_calibrator_view").unwrap();
     let button_calibrator_cancel: gtk::Button = builder.get_object("button_calibrator_cancel").unwrap();
     let button_messpunkt_nullgas: gtk::Button = builder.get_object("button_messpunkt_nullgas").unwrap();
-    let kombisensor_no2 = kombisensor.clone();
-    let kombisensor = kombisensor.lock().unwrap();
 
     match sensor_type.as_ref() {
         "NO2" => {
-            {
-                let ref sensor = kombisensor.sensors[0];
-                let label_sensor_type: gtk::Label = builder.get_object("label_sensor_type").unwrap();
-                let adjustment_sensor_minimal: gtk::Adjustment = builder.get_object("adjustment_sensor_minimal").unwrap();
-                let adjustment_sensor_maximal: gtk::Adjustment = builder.get_object("adjustment_sensor_maximal").unwrap();
-                let adjustment_sensor_concentration_at_nullgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_nullgas").unwrap();
-                let adjustment_sensor_concentration_at_messgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_messgas").unwrap();
-                label_sensor_type.set_text("Nemoto™ EC NAP-550 - NO2");
-                adjustment_sensor_minimal.configure(sensor.get_min_value() as f64,
-                    sensor.get_min_value() as f64, sensor.get_max_value() as f64 + 1.0, 1.0, 1.0, 1.0);
-                adjustment_sensor_maximal.configure(sensor.get_max_value() as f64 + 1.0,
-                    sensor.get_min_value() as f64, sensor.get_max_value() as f64 + 1.0, 1.0, 1.0, 1.0);
-                adjustment_sensor_concentration_at_nullgas.configure(0.0,
-                    sensor.get_min_value() as f64, sensor.get_max_value() as f64 + 1.0, 1.0, 1.0, 1.0);
-                adjustment_sensor_concentration_at_messgas.configure(sensor.get_concentration_at_messgas() as f64,
-                    sensor.get_min_value() as f64, sensor.get_max_value() as f64 + 1.0, 1.0, 1.0, 1.0);
-            }
-
-            button_messpunkt_nullgas.connect_clicked(clone!(builder, kombisensor_no2 => move |_| {
-                callback_button_messpunkt_nullgas("NO2", &builder, &kombisensor_no2);
-            }));
+            fill_widgets(&builder, &kombisensor, 0);
         },
         "CO" => {
-            let ref sensor = kombisensor.sensors[1];
-            let label_sensor_type: gtk::Label = builder.get_object("label_sensor_type").unwrap();
-            let adjustment_sensor_minimal: gtk::Adjustment = builder.get_object("adjustment_sensor_minimal").unwrap();
-            let adjustment_sensor_maximal: gtk::Adjustment = builder.get_object("adjustment_sensor_maximal").unwrap();
-            let adjustment_sensor_concentration_at_nullgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_nullgas").unwrap();
-            let adjustment_sensor_concentration_at_messgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_messgas").unwrap();
-            label_sensor_type.set_text("Nemoto™ EC NAP-505 - CO");
-            adjustment_sensor_minimal.configure(sensor.get_min_value() as f64,
-                sensor.get_min_value() as f64, sensor.get_max_value() as f64 + 1.0, 1.0, 1.0, 1.0);
-            adjustment_sensor_maximal.configure(sensor.get_max_value() as f64 + 1.0,
-                sensor.get_min_value() as f64, sensor.get_max_value() as f64 + 1.0, 1.0, 1.0, 1.0);
-            adjustment_sensor_concentration_at_nullgas.configure(0.0,
-                sensor.get_min_value() as f64, sensor.get_max_value() as f64 + 1.0, 1.0, 1.0, 1.0);
-            adjustment_sensor_concentration_at_messgas.configure(sensor.get_concentration_at_messgas() as f64,
-                sensor.get_min_value() as f64, sensor.get_max_value() as f64 + 1.0, 1.0, 1.0, 1.0);
-
+            fill_widgets(&builder, &kombisensor, 1);
         },
         _ => {}
     }
 
+    // Stack Layer anzeigen
     stack_main.set_visible_child(&box_calibrator_view);
 
-
+    // Zurueck Action
     button_calibrator_cancel.connect_clicked(move |_| {
         stack_main.set_visible_child(&box_index_view);
     });
