44b6f267e93aeedb0a70615729bec4c1d21ac755
Stefan Müller <s.mueller@it.kls-glt.de>
2016-11-11 10:01:32 +0100

    Erste Version des Refactor Zyklus 'SensorType'

--- src/gui/gtk3/view_calibrator.rs
+++ src/gui/gtk3/view_calibrator.rs
@@ -7,7 +7,6 @@ use gtk::prelude::*;
 
 
 fn fill_widgets(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>, sensor_num: usize) {
-    let button_messpunkt_nullgas: gtk::Button = builder.get_object("button_messpunkt_nullgas").unwrap();
     let mut sensor_type: &str = "";
     let label_sensor_type: gtk::Label = builder.get_object("label_sensor_type").unwrap();
     if sensor_num == 0 {
@@ -34,14 +33,23 @@ fn fill_widgets(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>, s
         adjustment_sensor_concentration_at_messgas.configure(sensor.get_concentration_at_messgas() as f64,
             min_value, max_value, 1.0, 1.0, 1.0);
     }
-
-    button_messpunkt_nullgas.connect_clicked(clone!(builder, kombisensor => move |_| {
-        callback_button_messpunkt_nullgas(&sensor_type, &builder, &kombisensor);
-    }));
 }
 
-fn callback_button_messpunkt_nullgas<T: AsRef<str>>(sensor_type: T, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
-    ::gui::gtk3::view_messpunkt::launch(sensor_type, &builder, &kombisensor);
+fn button_messpuntk_nullgas(sensor_type: SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
+    let button_messpunkt_nullgas: gtk::Button = builder.get_object("button_messpunkt_nullgas").unwrap();
+
+    match sensor_type {
+        SensorType::RaGasNO2 => {
+            button_messpunkt_nullgas.connect_clicked(clone!(builder, kombisensor => move |_| {
+                callback_button_messpunkt_nullgas(SensorType::RaGasNO2, &builder, &kombisensor);
+            }));
+        }
+        SensorType::RaGasCO => {}
+    }
+}
+
+fn callback_button_messpunkt_nullgas(sensor_type: SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
+    ::gui::gtk3::view_messpunkt::launch(sensor_type, "Nullgas", &builder, &kombisensor);
 }
 
 pub fn launch<T: AsRef<str>>(sensor_type: T, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) -> Result<(), CalibError> {
@@ -54,9 +62,13 @@ pub fn launch<T: AsRef<str>>(sensor_type: T, builder: &gtk::Builder, kombisensor
     match sensor_type.as_ref() {
         "NO2" => {
             fill_widgets(&builder, &kombisensor, 0);
+
+            button_messpuntk_nullgas(SensorType::RaGasNO2, &builder, &kombisensor);
         },
         "CO" => {
             fill_widgets(&builder, &kombisensor, 1);
+
+            button_messpuntk_nullgas(SensorType::RaGasCO, &builder, &kombisensor);
         },
         _ => {}
     }
--- src/gui/gtk3/view_messpunkt.rs
+++ src/gui/gtk3/view_messpunkt.rs
@@ -2,10 +2,11 @@ use gtk;
 use gtk::prelude::*;
 use std::sync::{Arc, Mutex};
 use std::borrow::Borrow;
+use co_no2_kombisensor::sensor::{SensorType};
 use co_no2_kombisensor::kombisensor::{Kombisensor};
 
 
-fn save_adc_value(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
+fn save_adc_value<T: AsRef<str>>(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>, sensor_num: usize, messpunkt_type: T) {
     let button_messpunkt_save: gtk::Button = builder.get_object("button_messpunkt_save").unwrap();
     button_messpunkt_save.connect_clicked(clone!(builder, kombisensor => move |_| {
         let check_button_adc_manuell: gtk::CheckButton = builder.get_object("check_button_adc_manuell").unwrap();
@@ -22,14 +23,14 @@ fn save_adc_value(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>)
             // Die Kombisensor.-/ Sensor Structuren werden im Worker Task über Modbus abgeglichen
             let mut kombisensor = kombisensor.lock().unwrap();
             kombisensor.set_live_update(false);
-            adc_value = kombisensor.sensors[0].get_adc_value() as i32;
+            adc_value = kombisensor.sensors[sensor_num].get_adc_value() as i32;
         }
 
         ::commands::sensor_new_adc_at_nullgas(&kombisensor, adc_value);
     }));
 }
 
-fn update_widgets(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
+fn update_widgets(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>, sensor_num: usize) {
     gtk::timeout_add(100, clone!(kombisensor, builder => move || {
         let label_messpunkt_sensor_type: gtk::Label = builder.get_object("label_messpunkt_sensor_type").unwrap();
         let label_messpunkt_adc: gtk::Label = builder.get_object("label_messpunkt_adc").unwrap();
@@ -46,7 +47,7 @@ fn update_widgets(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>)
         let adjustment_sensor_adc_value_at: gtk::Adjustment = builder.get_object("adjustment_sensor_adc_value_at").unwrap();
         {
             let kombisensor = kombisensor.lock().unwrap();;
-            let adc_value = kombisensor.sensors[0].get_adc_value() as f64;
+            let adc_value = kombisensor.sensors[sensor_num].get_adc_value() as f64;
             adjustment_sensor_adc_value_at.set_value(adc_value);
         }
 
@@ -56,8 +57,8 @@ fn update_widgets(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>)
         let mut mv_value = String::new();
         {
             let kombisensor = kombisensor.lock().unwrap();
-            adc_value = kombisensor.sensors[0].get_adc_value().to_string();
-            mv_value = kombisensor.sensors[0].get_mv().to_string();
+            adc_value = kombisensor.sensors[sensor_num].get_adc_value().to_string();
+            mv_value = kombisensor.sensors[sensor_num].get_mv().to_string();
         }
         label_messpunkt_adc.set_text(&adc_value);
         label_messpunkt_mV.set_text(&mv_value);
@@ -71,7 +72,7 @@ fn update_widgets(builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>)
     }));
 }
 
-pub fn launch<T: AsRef<str>>(sensor_type: T, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
+pub fn launch(sensor_type: SensorType, messpunkt_type: &'static str, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
     let stack_main: gtk::Stack = builder.get_object("stack_main").unwrap();
     let button_messpunkt_cancel: gtk::Button = builder.get_object("button_messpunkt_cancel").unwrap();
     let box_calibrator_view: gtk::Box = builder.get_object("box_calibrator_view").unwrap();
@@ -82,18 +83,17 @@ pub fn launch<T: AsRef<str>>(sensor_type: T, builder: &gtk::Builder, kombisensor
     let mut kombisensor_liveupdate = kombisensor.lock().unwrap();
     kombisensor_liveupdate.set_live_update(true);
 
-    match sensor_type.as_ref() {
-        "NO2" => {
-            update_widgets(&builder, &kombisensor);
+    match sensor_type {
+        SensorType::RaGasNO2 => {
+            update_widgets(&builder, &kombisensor, 0);
 
-            save_adc_value(&builder, &kombisensor);
+            save_adc_value(&builder, &kombisensor, 0, &messpunkt_type);
         }
-        "CO" => {
-            update_widgets(&builder, &kombisensor);
+        SensorType::RaGasCO => {
+            update_widgets(&builder, &kombisensor, 1);
 
-            save_adc_value(&builder, &kombisensor);
+            save_adc_value(&builder, &kombisensor, 1, &messpunkt_type);
         }
-        _ => { }
     }
 
     // Weg zurück
