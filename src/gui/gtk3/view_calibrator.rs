f7a4e0e22d7bcc2c67b1ca2a369a29c70b4208eb
Stefan Müller <s.mueller@it.kls-glt.de>
2016-11-15 15:13:52 +0100

    2. Version Speichern des gesammten Kombisensors via Modbus auf die Hardware
    Immernoch nicht funktional

--- src/gui/gtk3/view_calibrator.rs
+++ src/gui/gtk3/view_calibrator.rs
@@ -10,8 +10,53 @@ use std::error::Error;
 use std::sync::{Arc, Mutex};
 
 
-fn fill_widgets(sensor_type: &SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
+fn callback_button_calibrator_save(sensor_type: &SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
+    let adjustment_sensor_concentration_at_messgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_messgas").unwrap();
+    let adjustment_sensor_concentration_at_nullgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_nullgas").unwrap();
+    let adjustment_sensor_maximal: gtk::Adjustment = builder.get_object("adjustment_sensor_maximal").unwrap();
+    let adjustment_sensor_minimal: gtk::Adjustment = builder.get_object("adjustment_sensor_minimal").unwrap();
+    let info_bar: gtk::InfoBar = builder.get_object("info_bar").unwrap();
+    let label_info_bar_message: gtk::Label = builder.get_object("label_info_bar_message").unwrap();
     let label_sensor_type: gtk::Label = builder.get_object("label_sensor_type").unwrap();
+
+    let mut values: Vec<u16> = vec![0; 30];
+    let mut sensor_num: usize = 0;
+
+    match *sensor_type {
+        SensorType::RaGasNO2 => {
+            sensor_num = 0;
+        }
+        SensorType::RaGasCO => {
+            sensor_num = 1;
+        }
+    }
+
+    {
+        let mut kombisensor = kombisensor.lock().unwrap();
+        kombisensor.sensors[sensor_num].set_min_value(adjustment_sensor_minimal.get_value() as u16);
+        kombisensor.sensors[sensor_num].set_max_value(adjustment_sensor_maximal.get_value() as u16);
+        kombisensor.sensors[sensor_num].set_concentration_at_nullgas(adjustment_sensor_concentration_at_nullgas.get_value() as u16);
+        kombisensor.sensors[sensor_num].set_concentration_at_messgas(adjustment_sensor_concentration_at_messgas.get_value() as u16);
+
+        values = kombisensor.to_modbus_registers();
+    }
+
+    match ::commands::kombisensor_to_modbus(&kombisensor, &values) {
+        Ok(()) => {}
+        Err(err) => {
+            label_info_bar_message.set_text(err.description());
+            info_bar.show();
+        }
+    }
+}
+
+fn fill_widgets(sensor_type: &SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
+    let adjustment_sensor_concentration_at_messgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_messgas").unwrap();
+    let adjustment_sensor_concentration_at_nullgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_nullgas").unwrap();
+    let adjustment_sensor_maximal: gtk::Adjustment = builder.get_object("adjustment_sensor_maximal").unwrap();
+    let adjustment_sensor_minimal: gtk::Adjustment = builder.get_object("adjustment_sensor_minimal").unwrap();
+    let label_sensor_type: gtk::Label = builder.get_object("label_sensor_type").unwrap();
+
     let mut sensor_num: usize = 0;
 
     match *sensor_type {
@@ -27,18 +72,15 @@ fn fill_widgets(sensor_type: &SensorType, builder: &gtk::Builder, kombisensor: &
 
     let kombisensor = kombisensor.lock().unwrap();
     let ref sensor = kombisensor.sensors[sensor_num];
-    let adjustment_sensor_minimal: gtk::Adjustment = builder.get_object("adjustment_sensor_minimal").unwrap();
-    let adjustment_sensor_maximal: gtk::Adjustment = builder.get_object("adjustment_sensor_maximal").unwrap();
-    let adjustment_sensor_concentration_at_nullgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_nullgas").unwrap();
-    let adjustment_sensor_concentration_at_messgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_messgas").unwrap();
     let min_value = sensor.get_min_value() as f64;
     let max_value = sensor.get_max_value() as f64 + 1.0;
 
-    adjustment_sensor_minimal.configure(min_value, min_value, max_value, 1.0, 1.0, 1.0);
-    adjustment_sensor_maximal.configure(max_value, min_value, max_value, 1.0, 1.0, 1.0);
-    adjustment_sensor_concentration_at_nullgas.configure(0.0, min_value, max_value, 1.0, 1.0, 1.0);
+    adjustment_sensor_minimal.configure(min_value, 0.0, max_value + max_value * 0.5, 1.0, 1.0, 1.0);
+    adjustment_sensor_maximal.configure(max_value, 0.0, max_value + max_value * 0.5, 1.0, 1.0, 1.0);
+    adjustment_sensor_concentration_at_nullgas.configure(sensor.get_concentration_at_nullgas() as f64,
+        0.0, max_value + max_value * 0.5, 1.0, 1.0, 1.0);
     adjustment_sensor_concentration_at_messgas.configure(sensor.get_concentration_at_messgas() as f64,
-        min_value, max_value, 1.0, 1.0, 1.0);
+        0.0, max_value + max_value * 0.5, 1.0, 1.0, 1.0);
 }
 
 pub fn launch(sensor_type: SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) -> Result<(), CalibError> {
@@ -72,22 +114,8 @@ pub fn launch(sensor_type: SensorType, builder: &gtk::Builder, kombisensor: &Arc
         ::gui::gtk3::view_messpunkt::launch(GasType::Messgas, &sensor_type, &builder, &kombisensor);
     }));
 
-    let id_button_calibrator_save = button_calibrator_save.connect_clicked(clone!(builder, kombisensor => move |_| {
-        let info_bar: gtk::InfoBar = builder.get_object("info_bar").unwrap();
-        let label_info_bar_message: gtk::Label = builder.get_object("label_info_bar_message").unwrap();
-        let mut values: Vec<u16> = vec![0; 30];
-
-        {
-            let kombisensor = kombisensor.lock().unwrap();
-            values = kombisensor.to_modbus_registers();
-        }
-        match ::commands::kombisensor_to_modbus(&kombisensor, &values) {
-            Ok(()) => {}
-            Err(err) => {
-                label_info_bar_message.set_text(err.description());
-                info_bar.show();
-            }
-        }
+    let id_button_calibrator_save = button_calibrator_save.connect_clicked(clone!(builder, kombisensor, sensor_type => move |_| {
+        callback_button_calibrator_save(&sensor_type, &builder, &kombisensor);
     }));
 
     // Zurueck Action
