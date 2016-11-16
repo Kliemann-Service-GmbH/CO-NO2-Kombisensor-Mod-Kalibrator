68dc8fd7b92c82eb6f6898887c43f488ada4e49f
Stefan Müller <s.mueller@it.kls-glt.de>
2016-11-16 16:52:54 +0100

    Fix crash nach update der views

--- src/gui/gtk3/view_calibrator.rs
+++ src/gui/gtk3/view_calibrator.rs
@@ -10,45 +10,47 @@ use std::error::Error;
 use std::sync::{Arc, Mutex};
 
 
-fn callback_button_calibrator_save(sensor_type: &SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
-    let adjustment_sensor_concentration_at_messgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_messgas").unwrap();
-    let adjustment_sensor_concentration_at_nullgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_nullgas").unwrap();
-    let adjustment_sensor_maximal: gtk::Adjustment = builder.get_object("adjustment_sensor_maximal").unwrap();
-    let adjustment_sensor_minimal: gtk::Adjustment = builder.get_object("adjustment_sensor_minimal").unwrap();
-    let info_bar: gtk::InfoBar = builder.get_object("info_bar").unwrap();
-    let label_info_bar_message: gtk::Label = builder.get_object("label_info_bar_message").unwrap();
-    let label_sensor_type: gtk::Label = builder.get_object("label_sensor_type").unwrap();
+//fn callback_button_calibrator_view_save_min(sensor_type: &SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
+//    let adjustment_sensor_concentration_at_messgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_messgas").unwrap();
+//    let adjustment_sensor_concentration_at_nullgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_nullgas").unwrap();
+//    let adjustment_sensor_maximal: gtk::Adjustment = builder.get_object("adjustment_sensor_maximal").unwrap();
+//    let adjustment_sensor_minimal: gtk::Adjustment = builder.get_object("adjustment_sensor_minimal").unwrap();
+//    let info_bar: gtk::InfoBar = builder.get_object("info_bar").unwrap();
+//    let label_info_bar_message: gtk::Label = builder.get_object("label_info_bar_message").unwrap();
+//    let label_sensor_type: gtk::Label = builder.get_object("label_sensor_type").unwrap();
+//
+//    let mut values: Vec<u16> = vec![0; 30];
+//    let mut sensor_num: usize = 0;
+//
+//    match *sensor_type {
+//        SensorType::RaGasNO2 => {
+//            sensor_num = 0;
+//        }
+//        SensorType::RaGasCO => {
+//            sensor_num = 1;
+//        }
+//    }
+//
+//    {
+//        let mut kombisensor = kombisensor.lock().unwrap();
+//        kombisensor.sensors[sensor_num].set_min_value(adjustment_sensor_minimal.get_value() as u16);
+//        kombisensor.sensors[sensor_num].set_max_value(adjustment_sensor_maximal.get_value() as u16);
+//        kombisensor.sensors[sensor_num].set_concentration_at_nullgas(adjustment_sensor_concentration_at_nullgas.get_value() as u16);
+//        kombisensor.sensors[sensor_num].set_concentration_at_messgas(adjustment_sensor_concentration_at_messgas.get_value() as u16);
+//
+//        values = kombisensor.to_modbus_registers();
+//    }
+//
+//    match ::commands::kombisensor_to_modbus(&kombisensor, &values) {
+//        Ok(()) => {}
+//        Err(err) => {
+//            label_info_bar_message.set_text(err.description());
+//            info_bar.show();
+//        }
+//    }
+//}
 
-    let mut values: Vec<u16> = vec![0; 30];
-    let mut sensor_num: usize = 0;
-
-    match *sensor_type {
-        SensorType::RaGasNO2 => {
-            sensor_num = 0;
-        }
-        SensorType::RaGasCO => {
-            sensor_num = 1;
-        }
-    }
-
-    {
-        let mut kombisensor = kombisensor.lock().unwrap();
-        kombisensor.sensors[sensor_num].set_min_value(adjustment_sensor_minimal.get_value() as u16);
-        kombisensor.sensors[sensor_num].set_max_value(adjustment_sensor_maximal.get_value() as u16);
-        kombisensor.sensors[sensor_num].set_concentration_at_nullgas(adjustment_sensor_concentration_at_nullgas.get_value() as u16);
-        kombisensor.sensors[sensor_num].set_concentration_at_messgas(adjustment_sensor_concentration_at_messgas.get_value() as u16);
-
-        values = kombisensor.to_modbus_registers();
-    }
-
-    match ::commands::kombisensor_to_modbus(&kombisensor, &values) {
-        Ok(()) => {}
-        Err(err) => {
-            label_info_bar_message.set_text(err.description());
-            info_bar.show();
-        }
-    }
-}
+fn callback_button_calibrator_view_save_min(sensor_type: &SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {}
 
 fn fill_widgets(sensor_type: &SensorType, builder: &gtk::Builder, kombisensor: &Arc<Mutex<Kombisensor>>) {
     let adjustment_sensor_concentration_at_messgas: gtk::Adjustment = builder.get_object("adjustment_sensor_concentration_at_messgas").unwrap();
@@ -90,7 +92,7 @@ pub fn launch(sensor_type: SensorType, builder: &gtk::Builder, kombisensor: &Arc
     let button_calibrator_cancel: gtk::Button = builder.get_object("button_calibrator_cancel").unwrap();
     let button_messpunkt_nullgas: gtk::Button = builder.get_object("button_messpunkt_nullgas").unwrap();
     let button_messpunkt_messgas: gtk::Button = builder.get_object("button_messpunkt_messgas").unwrap();
-    let button_calibrator_save: gtk::Button = builder.get_object("button_calibrator_save").unwrap();
+    let button_calibrator_view_save_min: gtk::Button = builder.get_object("button_calibrator_view_save_min").unwrap();
 
     match sensor_type {
         SensorType::RaGasNO2 => {
@@ -114,8 +116,8 @@ pub fn launch(sensor_type: SensorType, builder: &gtk::Builder, kombisensor: &Arc
         ::gui::gtk3::view_messpunkt::launch(GasType::Messgas, &sensor_type, &builder, &kombisensor);
     }));
 
-    let id_button_calibrator_save = button_calibrator_save.connect_clicked(clone!(builder, kombisensor, sensor_type => move |_| {
-        callback_button_calibrator_save(&sensor_type, &builder, &kombisensor);
+    let id_button_calibrator_view_save_min = button_calibrator_view_save_min.connect_clicked(clone!(builder, kombisensor, sensor_type => move |_| {
+        callback_button_calibrator_view_save_min(&sensor_type, &builder, &kombisensor);
     }));
 
     // Zurueck Action
@@ -133,8 +135,8 @@ pub fn launch(sensor_type: SensorType, builder: &gtk::Builder, kombisensor: &Arc
             }
         }
         unsafe {
-            if gobject_ffi::g_signal_handler_is_connected(button_calibrator_save.to_glib_none().0, id_button_calibrator_save as c_ulong) == 1 {
-                gobject_ffi::g_signal_handler_disconnect(button_calibrator_save.to_glib_none().0, id_button_calibrator_save as c_ulong);
+            if gobject_ffi::g_signal_handler_is_connected(button_calibrator_view_save_min.to_glib_none().0, id_button_calibrator_view_save_min as c_ulong) == 1 {
+                gobject_ffi::g_signal_handler_disconnect(button_calibrator_view_save_min.to_glib_none().0, id_button_calibrator_view_save_min as c_ulong);
             }
         }
         stack_main.set_visible_child(&box_index_view);
